c1fb4222ceee64c829a4d2f57820314676eab074
MaxBachmann <maxbachmann2@gmail.com>
2018-08-07 23:25:54 +0200

    corrected endsession
    endsession command in wrong place so got always called

--- action-captn-MagicMirror2.py
+++ action-captn-MagicMirror2.py
@@ -46,12 +46,14 @@ def on_message(client, userdata, msg):
             module = slots['MODULE']
             action = {'module':module}
             MM2(intentname, action)
+            say(session_id, "Mache ich")
         if intentname == 'MM_Move':
             module = slots['MODULE']
             position = slots['POSITION']
             action = {'module':module, 'position':position}
             MM2(intentname, action)
-        say(session_id, "Mache ich")
+            say(session_id, "Mache ich")
+      
     except KeyError:
                 say(session_id, "Ich habe dich leider nicht verstanden.")
 
