228d7ac32fd68b43af2c68464a1fce6bcad5020f
MaxBachmann <maxbachmann2@gmail.com>
2018-08-13 17:38:34 +0200

    test

--- action-captn-MagicMirror2.py
+++ action-captn-MagicMirror2.py
@@ -72,8 +72,7 @@ def test(client, userdata, msg):
                 say(session_id, "Ich habe dich leider nicht verstanden.")
 
 def valve_callback(client, userdata, msg):
-    mqtt_client.publish(('hermes/external/test'),
-                        "Ich habe dich leider nicht verstanden." ))
+    mqtt_client.publish(('hermes/external/test'), "Ich habe dich leider nicht verstanden.")
 
 def MM2(intentname, action):
     mqtt_client.publish(('hermes/external/MagicMirror2/' + intentname),
