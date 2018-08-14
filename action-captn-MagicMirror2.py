a2ed843f85ea4e07ed91b5e7e82279585e674fea
MaxBachmann <maxbachmann2@gmail.com>
2018-08-14 09:47:16 +0200

    removed test

--- action-captn-MagicMirror2.py
+++ action-captn-MagicMirror2.py
@@ -32,48 +32,41 @@ print("Conf:", conf)
 mqtt_client = mqtt.Client()
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("hermes/intent/#")
-    client.subscribe("hermes/external/MagicMirror2/#")
-    client.subscribe("hermes/intent/#")
+    client.subscribe("hermes/intent/MM2-HideShowMove/#")
 
 
-def test(client, userdata, msg):
-    client.subscribe("hermes/intent/#")
+def message(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
     session_id = data['sessionId']
     try:
         slots = {slot['slotName']: slot['value']['value'] for slot in data['slots']}
         user, intentname = data['intent']['intentName'].split(':')
 
-        if (intentname == 'MM_Hide' or intentname == 'MM_Show' or intentname == 'MM_Move'):
-            module = slots['MODULE']
+        module = slots['MODULE']
 
-            if module == 'ALL':
-                mode = 'ALL'
-            elif 'PAGE' in module:
-                mode = 'PAGE'
-            else:
-                 mode = 'STANDARD'
+        if module == 'ALL':
+            mode = 'ALL'
+        elif 'PAGE' in module:
+            mode = 'PAGE'
+        else:
+            mode = 'STANDARD'
 
-            if intentname == 'MM_Hide' and (mode == 'STANDARD' or mode == 'ALL'):
-                action = {'module':module}
-            elif intentname == 'MM_Show' and (mode == 'STANDARD' or mode == 'PAGE') :
-                action = {'module':module}
-            elif intentname == 'MM_Move' and mode == 'STANDARD':
-                position = slots['POSITION']
-                action = {'module':module, 'position':position}
-            else:
-                raise UnboundLocalError("Das kann ich leider nicht")
-            say(session_id, "Mache ich")
-            MM2(intentname, action)
+        if intentname == 'MM_Hide' and (mode == 'STANDARD' or mode == 'ALL'):
+            action = {'module':module}
+        elif intentname == 'MM_Show' and (mode == 'STANDARD' or mode == 'PAGE') :
+            action = {'module':module}
+        elif intentname == 'MM_Move' and mode == 'STANDARD':
+            position = slots['POSITION']
+            action = {'module':module, 'position':position}
+        else:
+            raise UnboundLocalError("Das kann ich leider nicht")
+        say(session_id, "Mache ich")
+        MM2(intentname, action)
     except UnboundLocalError, e:
         say(session_id, e.message)
 
     except KeyError:
                 say(session_id, "Ich habe dich leider nicht verstanden.")
-
-def valve_callback(client, userdata, msg):
-    mqtt_client.publish(('hermes/external/test'), "Ich habe dich leider nicht verstanden.")
 
 def MM2(intentname, action):
     mqtt_client.publish(('hermes/external/MagicMirror2/' + intentname),
@@ -83,16 +76,11 @@ def say(session_id, text):
     mqtt_client.publish('hermes/dialogueManager/endSession',
                         json.dumps({'text': text, "sessionId": session_id}))
 
-def on_message(session_id, text):
-    pass
-
 
 
 
 if __name__ == "__main__":
     mqtt_client.on_connect = on_connect
-    mqtt_client.message_callback_add("hermes/intent/#", test)
-    mqtt_client.message_callback_add("hermes/external/MagicMirror2/#", valve_callback)
-    mqtt_client.on_message = on_message
+    mqtt_client.on_message = message
     mqtt_client.connect("localhost", "1883")
     mqtt_client.loop_forever()
